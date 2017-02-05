6ba5b96bce2fdd45defd7948f1dae44e16c4573c
Brandon Lou <brandon@Brandons-Mac.local>
2017-02-04 16:51:29 -0800

    Add random terrain

--- build/source/OriginalFractal.java
+++ build/source/OriginalFractal.java
@@ -1,11 +1,29 @@
-import processing.opengl.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
+
 
 /* Increase/decrease scale for number of triangles */
 public int scl = 20;
 
 /* Size of terrain */
-public int w = 900;
-public int h = 900;
+public int w = 750;
+public int h = 1000;
 
 /* Number in grid */
 public int cols = w/scl;
@@ -20,7 +38,7 @@ public float flying = 0;
 public int y = 0;
 
 public void setup() {
-  size(600,600,OPENGL);
+  
   frameRate(60);
 }
 
@@ -34,7 +52,7 @@ public void draw() {
   rotateX(PI/3);
   translate(-w/2,-h/2);
 
-  flying -= 0.1;
+  flying -= 0.1f;
   float yNoise = flying;
 
   for(int y = 0; y < rows; y++) {
@@ -44,10 +62,10 @@ public void draw() {
     for(int x = 0; x < cols; x++) {
       /* Add z coor after perlin noised to 2d array */
       terrain[x][y] = map(noise(xNoise, yNoise), 0, 1, -150, 150); // Change last two params for height
-      xNoise += 0.1; // change to make terrain more smooth/rough
+      xNoise += 0.1f; // change to make terrain more smooth/rough
     }
 
-    yNoise += 0.1; // change to make terrain more smooth/rough
+    yNoise += 0.1f; // change to make terrain more smooth/rough
 
   }
 
@@ -69,5 +87,15 @@ public void recurse(int x) {
     vertex(x*scl,y*scl,terrain[x][y]);
     vertex(x*scl,(y+1)*scl,terrain[x][y+1]);
     recurse(x+1);
+  }
+}
+  public void settings() {  size(600,600,OPENGL); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
